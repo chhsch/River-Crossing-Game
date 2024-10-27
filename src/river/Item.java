9097aa048a0bc8db6130a6919b96e5fef8bafd2a
Hathaway <hathaway1214@gmail.com>
2024-10-27 19:23:23 -0400

    completed project 2

---
+++ src/river/Item.java
@@ -0,0 +1,10 @@
+package river;
+
+public enum Item {
+    ITEM_0,  // Represents the first item (e.g., Farmer, Monster, etc.)
+    ITEM_1,
+    ITEM_2,
+    ITEM_3,
+    ITEM_4,  // Optional, used in specific games like MonsterGame
+    ITEM_5   // Optional, used in specific games like MonsterGame
+}
